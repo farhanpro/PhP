b1ef4089c848d7ce20d2a17cf502016850c9c808
farhanpro <shaikhfarhan549@gmail.com>
2020-02-10 00:07:00 +0530

    fourth day of PHP

--- Get.php
+++ Get.php
@@ -1,16 +1,6 @@
 <?php
-
-if(isset($_GET["Stud_Name"]) && isset($_GET["Stud_Age"]) && isset ($_Get["Stud_Weight"]))
-{
-	$Name =$_GET["Stud_Name"];
-	$Age =$_GET["Stud_Age"];
-	$Weight =$_GET["Stud_Weight"];
-	
-if(!empty($Name) && !empty($Age) && !empty($Weight))
-
-	echo 'Name : ' .$_GET["Stud_Name"].'<br>'; 
-	echo 'Age : ' .$_GET["Stud_Age"].'<br>';
-	echo 'Weight : ' .$_GET["Stud_Weight"].'<br>';
-
-}
+echo 'Name : '.$_GET["stu_name"].'<br>';
+echo 'Age : '.$_GET["Stud_Age"].'<br>';
+echo 'Weight : '.$_GET["Stud_Weight"].'<br>';
+echo 'Password : '.$_GET["Password"].'<br>';
 ?>
--- student.php
+++ student.php
@@ -2,11 +2,11 @@
 <body>
 <H1> Student Info </H1>
 <H2>
-<form action = "Get.php" method = "get">
-<b> Name : </b> <input type = "text" name = "Stud_Name"><br>
+<form action = "get.php" method = "get">
+<b> Name : </b> <input type = "text" name = "stu_name"><br>
 <b> Age  : </b> <input type = "text" name =  "Stud_Age"><br>
 <b> Weight  : </b> <input type = "text" name = "Stud_Weight"><br>
-<br>
+<b> Password :</b> <input type = "Password" name ="Password"><br>
 <input type = "submit">
 </H2>
 </body>
