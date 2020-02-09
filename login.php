f89901678444d74321d58d1286d80cd16084fe2e
farhanpro <shaikhfarhan549@gmail.com>
2020-02-10 00:31:36 +0530

    variable

---
+++ login.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<H1> Login </H1>
+<form action = "set.php" method = "get">
+<b>  Username <input type = "text" name = "Name"><br>
+<b> Password <input type = "Password" name = "Password"><br>
+<input type = "submit">
+</body>
+</html> 
---
+++ set.php
@@ -0,0 +1,4 @@
+<?php
+echo ' Name : '.$_GET["Name"].'<br>';
+echo ' Password : '.$_GET["Password"].'<br>';
+?>
