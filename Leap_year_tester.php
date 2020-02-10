6aa2ddc5d8682834b34c5ac30cfc80404310444d
farhanpro <shaikhfarhan549@gmail.com>
2020-02-11 01:54:01 +0530

    Fifth day

---
+++ Leap_year_tester.php
@@ -0,0 +1,12 @@
+<?php
+$Year = 2004;
+if($Year % 4 == 0 && $Year % 100 != 0)
+{
+	echo "Leap Year".'<br>';
+}
+else
+{
+	echo "Not the Leap Year";
+}
+	
+?>
---
+++ qw.php
@@ -0,0 +1,12 @@
+<?php
+$number =10;
+if($number % 5 == 0)
+{
+	echo "Divisible by five".'<br>';
+}
+else
+{
+	echo "Number is not divisible by five".'<br>';
+}
+	
+?>
